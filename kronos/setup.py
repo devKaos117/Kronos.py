4798448e27fa99a4c7132fa2caf8306ecfd73308
devKaos117 <gustavo.s.aragao.2003@gmail.com>
2025-04-29 00:59:47 -0300

    minor adjustments

--- kronos/setup.py
+++ kronos/setup.py
@@ -1,21 +1,14 @@
 from setuptools import setup, find_packages
 
-with open("../README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-with open("../LICENSE", "r", encoding="utf-8") as fh:
-    license_text = fh.read()
-
 setup(
     name="kronos",
     version="1.0.2",
     author="Gustavo Aragão",
     author_email="gustavo.s.aragao.2003@gmail.com",
     description="A python package to deal with time, analysis and logging",
-    long_description=long_description,
+    long_description="Kronos is a Python utility package for dealing with time, analysis and logging. It was designed to simplify the development of robust, high-performance applications with clean observability and controlled resource usage.",
     long_description_content_type="text/markdown",
     url="https://github.com/devKaos117/Kronos.py",
-    license=license_text,
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -24,15 +17,11 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.13",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: System :: Logging",
     ],
-    python_requires=">=3.8",
-    install_requires=[
-        "requests>=2.20.0",
-    ]
+    python_requires=">=3.8"
 )
